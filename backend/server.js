58ae6e7954aaa5bfea19e2c2f106207f5e4a2a35
Subharoy57 <145827552+Subharoy57@users.noreply.github.com>
2025-10-21 23:41:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(cors());
 // connection url
 
 const connection_url =
-"mongodb+srv://roy737970:admin@cluster0.g3cx0mt.mongodb.net/myDatabase"  
+"you database"  
 
 mongoose.connect(connection_url, {
   useNewUrlParser: true,
